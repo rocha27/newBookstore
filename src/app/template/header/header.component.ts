1dab636ee05ea224c053950085c749d7a7e00d34
Gabriel Rocha <rochasantos2702@gmail.com>
2022-10-05 02:20:32 -0300

    implementando header

--- src/app/template/header/header.component.ts
+++ src/app/template/header/header.component.ts
@@ -20,8 +20,8 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit(): void {
     this.items = [
-      {label: 'Home', icon: 'pi pi-fw pi-home'},
-      {label: 'Calendar', icon: 'pi pi-fw pi-calendar'},
+      {label: 'Home', icon: 'pi pi-fw pi-home', routerLink: ['/']},
+      {label: 'Categorias', icon: 'pi pi-fw pi-list', routerLink: ['/categorias']},
       {label: 'Edit', icon: 'pi pi-fw pi-pencil'},
       {label: 'Documentation', icon: 'pi pi-fw pi-file'},
       {label: 'Settings', icon: 'pi pi-fw pi-cog'}
